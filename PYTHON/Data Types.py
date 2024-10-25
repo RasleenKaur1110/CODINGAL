933b3855bd54a94c9b55ac1a282e7f711d89e64f
RasleenKaur1110 <krasleen11@gmail.com>
2024-10-25 21:02:11 +0530

    python

---
+++ PYTHON/Data Types.py
@@ -0,0 +1,13 @@
+name = "Rasleen"
+age = 13
+float = 38.5
+Boolean = True
+
+print("Name: ", name)
+print("Data Type of it is: ", type(name))
+print("Age: ", age)
+print("Data Type of it is: ", type(age))
+print("Float: ", float)
+print("Data Type of it is: ", type(float))
+print("Boolean: ", Boolean)
+print("Data Type of it is: ", type(Boolean))
---
+++ PYTHON/Python Operators.py
@@ -0,0 +1,18 @@
+num1 = 115
+num2 = 8
+
+print("Number 1 is: ", num1)
+print("Number 2 is: ", num2)
+print("Addition: ", num1+num2)
+print("Subtraction: ", num1-num2)
+print("Multiplication: ", num1*num2)
+print("Division: ", num1/num2)
+print("Floor Division: ", num1//num2)
+print("Modulus: ", num1%num2)
+print("Exponents: ", num1**num2)
+print("Square Root: ", num1**0.5)
+
+print("Checking if they are equal: ", num1==num2)
+print("Checking if Number 1 is greater than Number 2: ", num1>num2)
+print("Checking if Number 2 is greater than Number 1: ", num1<num2)
+print("Checking if they are not equal: ", num1!=num2)
