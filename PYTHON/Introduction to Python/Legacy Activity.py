3449606b4d30bc91a51c8587fd4b026db9e4f678
RasleenKaur1110 <krasleen11@gmail.com>
2024-10-26 17:09:57 +0530

    print

---
+++ PYTHON/Introduction to Python/Legacy Activity.py
@@ -0,0 +1,6 @@
+print("Welcome to the world of programming")
+print(15)
+print("HI EVERYONE \n ")
+print("Keywords in Python are....")
+import keyword
+print(keyword.kwlist)
---
+++ PYTHON/Introduction to Python/Print Statements and Comments.py
@@ -0,0 +1,11 @@
+#Greeting Message
+print("Hi Everyone")
+
+#Name
+print("My name is Rasleen")
+
+#Fun fact
+print("I love coding and reading fictional books")
+
+#Simple calculation
+print("Result of 11+15 is ", 11+15)
