9a963e975d03263f6bb6d528acc1f7e2b2c6edb5
RasleenKaur1110 <krasleen11@gmail.com>
2024-11-17 14:58:35 +0530

    list

---
+++ PYTHON/Data Structures in Python 2/Tuple to List.py
@@ -0,0 +1,6 @@
+#Adding my first name, last name, age, height, weight, favourite subject
+tuple1 = ("Rasleen", "Kaur", "13 years", "5'0 ft", "45 kg", "English")
+list1 = list(tuple1)
+
+print("Original Tuple: ", tuple1)
+print("Updated List: ", list1)
